71f4c2864e8bf0bcad4f391b6ece121920b44307
Alberto Caparrós <acaparros87@gmail.com>
2020-05-14 15:59:57 +0200

    readme

---
+++ src/totalScoreComponent.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { getTotalScore } from './averageService';
+const classes = require('./totalScoreComponent.scss');
+
+export const TotalScoreComponent: React.FunctionComponent = () => {
+  const [totalScore, setTotalScore] = React.useState<number>(0);
+  React.useEffect(() => {
+    const scores: number[] = [10, 20, 30, 40, 50];
+    setTotalScore(getTotalScore(scores));
+  }, []);
+  return (
+    <div>
+      <span className={classes.resultBackground}>
+        Students total score: {totalScore}
+      </span>
+    </div>
+  );
+};
